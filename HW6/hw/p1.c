8ae805cc9fafc7d590c9ffee378b37f212ec13f1
Vincent Portelli <vinport16@gmail.com>
2018-03-06 18:35:57 -0500

    made p1.c

---
+++ HW6/hw/p1.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <omp.h> 
+
+double shubert(double x1, double x2) {
+	double sum1 = 0; 
+	double sum2 = 0;
+	int i;
+	for (i = 1; i <= 5; i++) {
+		sum1 += i * cos((i + 1) * x1 + i);
+		sum2 += i * cos((i + 1) * x2 + i);
+	}
+	return sum1 * sum2;
+}
+
+int main (){
+	int x1;
+	int x2, y, min = 9999;
+
+	#pragma omp parallel for 
+	for(x1 = -4; x1 <= 4; x1++){
+		for (x2 = -2; x2 <= 2; x2++){
+			y = shubert(x1/2.0, x2);
+			#pragma omp critical
+			if(y < min){
+				min = y;
+				printf("new min: %d; found by: %f", min, omp_get_thread_num());
+			}
+		}
+	}
+
+}
