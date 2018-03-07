6b73c3ed727fe58598deb8e7ee3983381d8fc57c
Vincent Portelli <vinport16@gmail.com>
2018-03-06 21:42:24 -0500

    added hw3.c

---
+++ HW6/hw/p3.c
@@ -0,0 +1,92 @@
+#include <unistd.h> // For fork and shared memory
+#include <stdio.h> // For printf
+#include <stdlib.h> // For exit
+#include <fcntl.h> // For shared memory
+#include <sys/shm.h> // For shared memory
+#include <sys/mman.h> // For shared memory
+#include <sys/wait.h> // For wait
+#include <semaphore.h> // For semaphores
+
+double shubert(double x1, double x2) {
+	double sum1 = 0; 
+	double sum2 = 0;
+	int i;
+	for (i = 1; i <= 5; i++) {
+		sum1 += i * cos((i + 1) * x1 + i);
+		sum2 += i * cos((i + 1) * x2 + i);
+	}
+	return sum1 * sum2;
+}
+
+double min = 9999;
+
+int main (){
+
+	const int SIZE = 1;
+	const char *name = "Vincent's C00L Memory";
+	int shm_fd;
+	sem_t *sem;
+	// Create shared memory for semaphore
+	shm_fd = shm_open(name, O_CREAT | O_RDWR, 0666);
+	ftruncate(shm_fd,SIZE);
+	sem = mmap(0,SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+	if (sem == MAP_FAILED) {
+		printf("Map failed\n");
+		exit(0);
+	}
+	// Set up semaphore
+	if(sem_init(sem, 1, 1) < 0) { // 1 = multiprocess
+		fprintf(stderr, "ERROR: could not initialize semaphore.\n");
+		exit(0);
+	}
+
+	pid_t pid;
+	pid = fork();
+	if (pid == 0) {
+		int x1;
+		double x2, y;
+
+		for(x1 = -4; x1 <= 0; x1++){
+			for (x2 = -2; x2 <= 2; x2++){
+				y = shubert(x1/2.0, x2);
+				sem_wait(sem);
+				if(y < min){
+					min = y;
+					printf("Child found new min: %f\n", min);
+					fflush(stdout);
+				}
+				sem_post(sem);
+			}
+		}
+	}
+	else {
+
+		int x1;
+		double x2, y;
+
+		for(x1 = 1; x1 <= 4; x1++){
+			for (x2 = -2; x2 <= 2; x2++){
+				y = shubert(x1/2.0, x2);
+				sem_wait(sem);
+				if(y < min){
+					min = y;
+					printf("Parent found new min: %f\n", min);
+					fflush(stdout);
+				}
+				sem_post(sem);
+			}
+		}
+
+		wait(NULL); // Need to wait because someone needs to clean up
+		sem_destroy(sem); // Clean up semaphore
+
+		if (shm_unlink(name) == -1) { // Clean up shared memory
+			printf("Error removing %s\n",name);
+			exit(0);
+		}
+
+		printf("\nMinimum = %f\n", min);
+
+	}
+
+}
