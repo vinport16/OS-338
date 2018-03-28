17e3b8fc294d983ac665bf514d311a44f4098f40
Vincent Portelli <vinport16@gmail.com>
2018-03-27 22:03:52 -0400

    hw7

---
+++ HW7/p1.c
@@ -0,0 +1,78 @@
+#include <string.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>     
+#include <sys/types.h>  
+#include <stdlib.h>     
+#include <semaphore.h>  
+
+#define N 10
+#define BUFFER_SIZE 5
+
+/* Global variables are shared by the thread(s) */
+char buffer[BUFFER_SIZE];
+sem_t mutex, full, empty;
+
+void *child(); /* child thread */
+
+int main(int argc, char *argv[]) {
+
+//initializng the semaphore
+if(sem_init(&mutex, 0, 1) < 0 || sem_init(&full, 0, 0) < 0 || sem_init(&empty, 0, BUFFER_SIZE) < 0) { // 0 = multithreaded
+fprintf(stderr, "ERROR: could not initialize &semaphore.\n");
+exit(0);
+}
+
+pthread_t tid; /* the thread identifiers */
+pthread_attr_t attr; /* set of attributes for the thread */
+pthread_attr_init(&attr);
+pthread_create(&tid, &attr, child, NULL);
+
+/*
+ Parent information (writes to buffer)
+ */
+int data[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100}; // Must have N values
+int i, input = 0;
+
+for (i = 0; i < N; i++){
+	sem_wait(&empty);
+	sem_wait(&mutex);
+
+	buffer[input] = data[i];
+
+	sem_post(&mutex);
+	sem_post(&full);
+
+	input = (input + 1) % BUFFER_SIZE;
+}
+
+// Join and print result
+pthread_join(tid, NULL);
+sem_destroy(&mutex);
+sem_destroy(&full);
+sem_destroy(&empty);
+}
+
+void *child() {
+
+	/*
+	 Child information (retrieves from buffer)
+	 */
+
+	int i, output = 0;
+
+	for (i = 0; i < N; i++){
+	sem_wait(&full);
+	sem_wait(&mutex);
+
+	int data = buffer[output];
+	printf("%d\n", data);
+	
+	sem_post(&mutex);
+	sem_post(&empty);
+
+	//increment buffer pointer
+	output = (output + 1) % BUFFER_SIZE;
+	}
+	pthread_exit(0);
+}
---
+++ HW7/p2.c
@@ -0,0 +1,88 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h> 
+#include <sys/sem.h>    
+#include <sys/types.h>  
+#include <stdlib.h>     
+#include <semaphore.h>  
+#include <string.h>
+
+#define BUFFER_SIZE 20
+
+char buffer[BUFFER_SIZE];
+sem_t rw_mutex, mutex;
+int version = 0;
+int read_count = 0;
+
+void *child();
+
+int main(int argc, char *argv[]) {
+	if(sem_init(&rw_mutex, 0, 1) < 0 || sem_init(&mutex, 0, 1) < 0) { // 0 = multithreaded
+		fprintf(stderr, "ERROR: could not initialize semaphore.\n");
+		exit(0);
+	}
+
+	pthread_t tid1, tid2;
+	pthread_create(&tid1, NULL, child, NULL);
+	pthread_create(&tid2, NULL, child, NULL);
+
+	sem_wait(&rw_mutex);
+	sprintf(buffer, "%s", "Roses are red");
+	version = 1;
+	sem_post(&rw_mutex);
+	sleep(1);
+
+	sem_wait(&rw_mutex);
+	sprintf(buffer, "%s", "Protests are great");
+	version = 2;
+	sem_post(&rw_mutex);
+	sleep(1);
+
+	sem_wait(&rw_mutex);
+	sprintf(buffer, "%s", "Before we get gun control");
+	version = 3;
+	sem_post(&rw_mutex);
+	sleep(1);
+
+	sem_wait(&rw_mutex);
+	sprintf(buffer, "%s", "Disarm the state");
+	version = 4;
+	sem_post(&rw_mutex);
+	sleep(1);
+
+// Join
+	pthread_join(tid1, NULL);
+	pthread_join(tid2, NULL);
+	sem_destroy(&rw_mutex);
+	sem_destroy(&mutex);
+
+}
+
+void *child() {
+	pthread_t tid = pthread_self();
+	int child_version = 0;
+
+	while(child_version != 4){
+		sem_wait(&mutex);
+		read_count++;
+		if (read_count == 1) {
+				sem_wait(&rw_mutex);
+		}
+		sem_post(&mutex);
+
+		if (child_version != version){
+			printf("%d: %s\n", (int)tid, buffer);
+			child_version = version;
+		}
+
+		sem_wait(&mutex);
+		read_count--;
+
+		if (read_count == 0){
+			sem_post(&rw_mutex);
+		}
+		sem_post(&mutex);
+
+	}
+	pthread_exit(0); 
+}
