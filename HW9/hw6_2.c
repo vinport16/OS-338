a31ea3a466794ba06d7b33c079d0d364f5360b5b
Vincent Portelli <vinport16@gmail.com>
2018-04-12 16:22:59 -0400

    disk

--- HW9/hw6_2.c
+++ HW9/hw6_2.c
@@ -31,7 +31,8 @@ int main(int argc, char *argv[])
 	tids = malloc(sizeof(pthread_t)*threadnum);
 	int i;
 	for(i = 0; i < threadnum; i++){
-		pthread_create(&tids[i], NULL, child, -2.0 + i * 4.0 / threadnum);
+		int q = (-2.0 + i * 4.0 / threadnum)
+		pthread_create(&tids[i], NULL, child, &q);
 		printf("created thread %d\n", threadnum);
 	}
 	
