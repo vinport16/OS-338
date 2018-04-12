e0e9c678e20f69ae80bcd31f31c5947704d2f422
Vincent Portelli <vinport16@gmail.com>
2018-04-12 16:20:16 -0400

    osfdk

--- HW9/hw6_2.c
+++ HW9/hw6_2.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 	// Prepare the child thread
 	pthread_t *tids;
 	tids = malloc(sizeof(pthread_t)*threadnum);
-
+	int i;
 	for(i = 0; i < threadnum; i++){
 		pthread_create(&tids[i], NULL, child, -2.0 + i * 4.0 / threadnum);
 		printf("created thread %d\n", threadnum);
